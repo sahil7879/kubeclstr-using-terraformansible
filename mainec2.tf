2d5b33f17b7cf32e87881d154e5f8bd1cf516408
sahil7879 <43738848+sahil7879@users.noreply.github.com>
2025-06-12 12:34:02 +0530

    Update mainec2.tf

--- mainec2.tf
+++ mainec2.tf
@@ -8,7 +8,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-24.04-amd64-server-*"]
   }
 
   filter {
